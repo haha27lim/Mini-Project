462bd8b63f5bf29836449f874307805b90cec556
haha27lim <eileen2708@hotmail.com>
2023-05-27 04:41:06 +0800

    updated

--- client/src/app/constants.ts
+++ client/src/app/constants.ts
@@ -17,8 +17,8 @@ export const TYPES = [
     "drink"
 ]
 
-export const BASE_URL = "http://localhost:8080"
-// export const BASE_URL = "https://typical-deer-production.up.railway.app"
+// export const BASE_URL = "http://localhost:8080"
+export const BASE_URL = "https://typical-deer-production.up.railway.app"
 
-export const WS_BASE_URL = "localhost:8080"
-// export const WS_BASE_URL = "typical-deer-production.up.railway.app"
+// export const WS_BASE_URL = "localhost:8080"
+export const WS_BASE_URL = "typical-deer-production.up.railway.app"
