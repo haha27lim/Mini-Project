c2fb12e8bdefa0fa581e13203b68a4642e273297
haha27lim <eileen2708@hotmail.com>
2023-05-29 05:24:51 +0800

    updated localstorage for token

--- client/src/app/services/token-storage.service.ts
+++ client/src/app/services/token-storage.service.ts
@@ -11,25 +11,26 @@ export class TokenStorageService {
   constructor() { }
 
   signOut(): void {
-    window.sessionStorage.clear();
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.removeItem(USER_KEY);
   }
 
   public saveToken(token: string): void {
-    window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.setItem(TOKEN_KEY, token);
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.setItem(TOKEN_KEY, token);
   }
 
   public getToken(): string {
-    return sessionStorage.getItem(TOKEN_KEY) as string;
+    return localStorage.getItem(TOKEN_KEY) as string;
   }
 
   public saveUser(user:any): void {
-    window.sessionStorage.removeItem(USER_KEY);
-    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+    window.localStorage.removeItem(USER_KEY);
+    window.localStorage.setItem(USER_KEY, JSON.stringify(user));
   }
 
   public getUser(): any {
-    return JSON.parse(sessionStorage.getItem(USER_KEY) as string) ;
+    return JSON.parse(localStorage.getItem(USER_KEY) as string) ;
   }
 
   public getUserId(): number | null {
