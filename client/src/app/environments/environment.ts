b92ff33ee0da0ed80b02764c79a68817a7f324bd
haha27lim <eileen2708@hotmail.com>
2023-05-29 18:30:22 +0800

    updated

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 
 export const environment = {
-  production: false,
+  production: true,
   url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/images/analyze',
   headers: new HttpHeaders({
     'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
