4d9fe23d9570ae8695baecc1601ac149e4990c56
haha27lim <eileen2708@hotmail.com>
2025-01-24 03:14:09 +0800

    add @ngx-env/builder and amend environment.ts

--- client/src/app/environments/environment.prod.ts
+++ client/src/app/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/images/analyze',
   headers: new HttpHeaders({
     'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-    'x-rapidapi-key': process.env['RAPIDAPI_KEY'] || ''
+    'x-rapidapi-key': import.meta.env['NG_APP_RAPIDAPI_KEY']
   })
 };
